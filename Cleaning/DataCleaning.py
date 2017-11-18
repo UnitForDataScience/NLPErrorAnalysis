b96fb52182bc834be95b4233513e48595b8d0690
Henry <henry@henrys-mac-pro.dhcp.asu.edu>
2017-11-18 13:36:48 -0700

    Working on that data cleaning. Since, we have good data source we might end up spending less time on this

--- Cleaning/DataCleaning.py
+++ Cleaning/DataCleaning.py
@@ -1,22 +1,19 @@
 from pyspark import SparkContext
 from pyspark import SparkConf
-from nltk import sent_tokenize
-
-# Fetching the data from the files
-# folderPath = '../Data/'
-# files = [open(join(folderPath, f)).read().decode("utf-8").strip() for f in listdir(folderPath) if
-#          isfile(join(folderPath, f))]
-#
-# # Collecting the sentences
-# sentences = []
-# for file in files:
-#     for sentence in sent_tokenize(file):
-#         sentences.append(sentence)
-# print len(sentences)
+from nltk import sent_tokenize, word_tokenize
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer
 
 # Using Native Spark to do the job as this will scale later
 conf = SparkConf().setAppName("Data cleaner").setMaster("local[*]")
 sc = SparkContext(conf=conf)
+lemmatizer = WordNetLemmatizer()
 
-print len(sc.textFile('../Data/').map(lambda x: x.strip())
-          .flatMap(lambda x: sent_tokenize(x)).collect())
+sentences = sc.textFile('../Data/').map(lambda x: x.strip()) \
+    .flatMap(lambda x: sent_tokenize(x)).map(lambda x: (x, pos_tag(word_tokenize(x)), word_tokenize(x))).collect()
+
+for (sentence, taggedData, lemmatizedWords) in sentences:
+    print '------------------------------------'
+    print sentence
+    print taggedData
+    print tokenizedWords
