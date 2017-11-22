d9085bd555f4d1528b3470b32cd3d4d53afafdd2
Henry <henry@henrys-mac-pro.dhcp.asu.edu>
2017-11-22 16:17:33 -0700

    used chunking and chinking put the results are still not that satisfactory, will have to wait what sally has to give and then we can use that information to code the things inside the system

--- Cleaning/DataCleaning.py
+++ Cleaning/DataCleaning.py
@@ -3,17 +3,56 @@ from pyspark import SparkConf
 from nltk import sent_tokenize, word_tokenize
 from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
+from nltk import RegexpParser
 
 # Using Native Spark to do the job as this will scale later
 conf = SparkConf().setAppName("Data cleaner").setMaster("local[*]")
 sc = SparkContext(conf=conf)
 lemmatizer = WordNetLemmatizer()
 
-sentences = sc.textFile('../Data/').map(lambda x: x.strip()) \
-    .flatMap(lambda x: sent_tokenize(x)).map(lambda x: (x, pos_tag(word_tokenize(x)), word_tokenize(x))).collect()
 
-for (sentence, taggedData, lemmatizedWords) in sentences:
+def chunkExtractor(t):
+    print t.node
+    return True
+
+
+def chinker(taggedWords):
+    chunkGram = r"""Chunk: {<.*>+}
+                            }<CC|CD|EX|FW|IN|DT>+{"""
+    chunkParser = RegexpParser(chunkGram)
+    chunked = chunkParser.parse(taggedWords)
+    chunkedData = [tree for tree in chunked.subtrees() if tree.label() == "Chunk"]
+    print chunkedData
+
+
+def chunker(taggedWords):
+    chunkGram = r"""Chunk: {<NNP.?>+}"""
+    chunkParser = RegexpParser(chunkGram)
+    chunked = chunkParser.parse(taggedWords)
+    print chunked
+
+
+# This is the sentencemapper which will do the cleaning of the sentence
+def sentenceMapper(sentence):
+    tokenizedWords = word_tokenize(sentence)
+    newWordList = []
+    for word in tokenizedWords:
+        if word.isupper():
+            newWordList.append(
+                word[0:1].encode("utf-8").upper() + "" + word[1:].encode("utf-8").lower())
+        else:
+            newWordList.append(word)
+    posTagged = pos_tag(newWordList)
+    return (sentence, posTagged, newWordList)
+
+
+sentences = sc.textFile('../Data/').map(lambda x: x.strip()) \
+    .flatMap(lambda x: sent_tokenize(x)).map(lambda x: x).map(sentenceMapper)
+
+for (sentence, taggedData, lemmatizedWords) in sentences.collect():
     print '------------------------------------'
-    print sentence
-    print taggedData
-    print tokenizedWords
+    # print sentence
+    # print taggedData
+    chinker(taggedData)
+    raw_input("Press any key to continue")
+# print lemmatizedWords
