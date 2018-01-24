7d485da38a0250ccffa51e8e61a54cdad5420b79
Henry <henry@henrys-mac-pro.dhcp.asu.edu>
2018-01-24 13:29:56 -0700

    Working on efficent phrase extractor module

--- Cleaning/ExtractPhrases_InspectionReport.py
+++ Cleaning/ExtractPhrases_InspectionReport.py
@@ -2,6 +2,7 @@ from os import listdir
 from collections import Counter
 from nltk import ngrams
 from nltk import sent_tokenize, word_tokenize
+from pyspark import SparkContext, SparkConf
 
 phrase_counter = Counter()
 file_path = "../Data/Inspection_Report/"
@@ -14,22 +15,56 @@ def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 
 
-random_stop_words = ["...", 'nrc', 'public']
+documents = []
 
 for possible_directory in listdir(file_path):
-    if ".html" in possible_directory:
+    if '.html' in possible_directory:
         continue
     inspection_report_path = file_path + possible_directory + "/"
-    print(possible_directory)
     for file in listdir(inspection_report_path):
-        sentences = sent_tokenize(_removeNonAscii(open(inspection_report_path + file, 'r').read()).lower())
-        for sentence in sentences:
-            filtered_sentence = [w for w in word_tokenize(sentence) if w not in random_stop_words]
-            for phrase in ngrams(filtered_sentence, 10):
-                phrase_counter[phrase] += 1
-    if
-    break
+        documents.append(inspection_report_path + file)
+random_stop_words = ["...", 'nrc', 'public']
 
-most_common_phrases = phrase_counter.most_common(1000)
-for k, v in most_common_phrases:
-    print '{0: <5}'.format(v), k
+print documents
+# for possible_directory in listdir(file_path):
+#     if ".html" in possible_directory:
+#         continue
+#     inspection_report_path = file_path + possible_directory + "/"
+#     print(possible_directory)
+#     for file in listdir(inspection_report_path):
+#         sentences = sent_tokenize(_removeNonAscii(open(inspection_report_path + file, 'r').read()).lower())
+#         for sentence in sentences:
+#             filtered_sentence = [w for w in word_tokenize(sentence) if w not in random_stop_words]
+#             for phrase in ngrams(filtered_sentence, 10):
+#                 phrase_counter[phrase] += 1
+#
+#     break
+#
+
+
+conf = SparkConf().setAppName("Data cleaner").setMaster("local[*]").set('spark.executor.memory', '14G').set(
+    'spark.driver.memory', '160g').set('spark.driver.maxResultSize', '10G').set("spark.executor.heartbeatInterval",
+                                                                                "3600s")
+SparkContext.setSystemProperty("spark.driver.memory", "40gb")
+sc = SparkContext(conf=conf)
+
+phrase_counter = Counter()
+
+
+def set_phrase_counter(x):
+    filtered_sentence = [w for w in word_tokenize(x) if w not in random_stop_words]
+    return filtered_sentence
+
+
+rddDocuments = sc.parallelize(documents).map(
+    lambda x: sent_tokenize(_removeNonAscii(open(x, 'r').read()).lower())).map(
+    lambda x: [w for w in word_tokenize(x) if w not in random_stop_words]).collect()
+
+#     map(set_phrase_counter).flatMap(lambda x: x).collect()
+#
+# for item in rddDocuments:
+#     phrase_counter[iter] += 1
+#
+# most_common_phrases = phrase_counter.most_common(1000)
+# for k, v in most_common_phrases:
+#     print '{0: <5}'.format(v), k
